f46eb147a1622d3e1d6451c8e86641df48655a7d
weiyafei <weiyafei@cnstrong.cn>
2020-12-19 20:52:09 +0800

    最后一次次

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hello from './button/index'
 import MyButton from './ButtonCustom/button'
 import {Button} from 'antd'
 // 主题色
-console.log('最后一次修复线上BUg')
+console.log('最后一次修复线上BUg,完毕')
 
 interface ThemeProps {
   [key: string]: {color: string, background: string}
