b92576d7af1374923585e1c472be4679be5e6c2d
weiyafei <weiyafei@cnstrong.cn>
2020-12-19 21:06:32 +0800

    0

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import Hello from './button/index'
 import MyButton from './ButtonCustom/button'
 import {Button} from 'antd'
 // 主题色
-<<<<<<< Updated upstream
 console.log('最后一次修复线上BUg,完毕')
-=======
-console.log('继续写代码，又有问题了')
-console.log('还要修改一下')
-console.log('代码写完')
->>>>>>> Stashed changes
 
 interface ThemeProps {
   [key: string]: {color: string, background: string}
