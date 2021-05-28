bc073a922d358b86535f96b2b479bc0e9afa429e
weiyafei-add <yafei804109496@163.com>
2021-05-28 11:36:20 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
-import React, {useCallback, useEffect, useMemo}from 'react';
-import './App.less';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import PageOne from './tt/page1';
-import PageTwo from './tt/page2';
+import React, { useCallback, useEffect, useMemo } from "react";
+import "./App.less";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import PageOne from "./tt/page1";
+import PageTwo from "./tt/page2";
+import Animation from "./animation/index";
 
 // 主题色
 
 interface ThemeProps {
-  [key: string]: {color: string, background: string}
+  [key: string]: { color: string; background: string };
 }
 
 export const theme: ThemeProps = {
   light: {
-    color: '#000',
-    background: '#fff',
+    color: "#000",
+    background: "#fff",
   },
   drak: {
-    color: '#fff',
-    background: '#000'
-  }
-}
-export const themeContext = React.createContext(theme.light)
-const arr = new Array(5).fill('1')
-
+    color: "#fff",
+    background: "#000",
+  },
+};
+export const themeContext = React.createContext(theme.light);
+const arr = new Array(5).fill("1");
 
 function App() {
   return (
-    <div className='App'>
-                <DndProvider backend={HTML5Backend} >
-                    <PageOne></PageOne>
-                    <PageTwo></PageTwo>
-                 </DndProvider>
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <PageOne></PageOne>
+        <PageTwo></PageTwo>
+      </DndProvider>
+      <Animation />
     </div>
   );
 }
