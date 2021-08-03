e861b3bb2bd120cecafbaf3b8c1a79d20d6ece9b
weiyafei <weiyafei@cnstrong.cn>
2021-08-03 14:53:12 +0800

    feat: 新增requestanimation动画

--- src/REF/index.jsx
+++ src/REF/index.jsx
@@ -1,23 +1,25 @@
-import React, {useRef, useEffect, useState} from 'react'
-import Child from './Child'
+import React, { useRef, useEffect, useState } from "react";
+import Child from "./Child";
 
 const FancyButton = () => {
-    const [count, setCount] = useState(0)
-    const buttonRef = useRef(null)
-    useEffect(() => {
-        console.log(buttonRef.current)
-    })
-    const handleOnclick = (data) => {
-        if(data) {
-            buttonRef.current?.handleOnInput(data)
-            return
-        }
-        buttonRef.current.handleOnClick()
+  const [count, setCount] = useState(0);
+  const buttonRef = useRef(null);
+  useEffect(() => {
+    console.log(buttonRef.current);
+  });
+  const handleOnclick = (data) => {
+    if (data) {
+      buttonRef.current?.handleOnInput(data);
+      return;
     }
-    return <div>
-        <button onClick={() => handleOnclick(null)} >parentButton</button>
-        <button onClick={() => handleOnclick('aaa')} >传值Btn</button>
-        <Child count={count} ref={buttonRef} ></Child>
+    buttonRef.current.handleOnClick();
+  };
+  return (
+    <div>
+      <button onClick={() => handleOnclick(null)}>parentButton</button>
+      <button onClick={() => handleOnclick("aaa")}>传值Btn</button>
+      <Child count={count} ref={buttonRef}></Child>
     </div>
-}
-export default FancyButton
+  );
+};
+export default FancyButton;
--- src/animation/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-const Animation = () => {
-  return <div className={"animation"}></div>;
-};
-
-export default Animation;
